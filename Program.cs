bda45c6dbd2b599a469c1fd4640cacb202361aef
Marcos Paulo Ribeiro <31698863+mpr3421account@users.noreply.github.com>
2022-12-27 11:23:22 -0300

    final

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using demoInheritance.Entities;
 
 BusinessAccount businessAccount = new BusinessAccount(8010, "Marcos", 1000.00, 500.00);
-Console.WriteLine(businessAccount.Balance);
+Console.WriteLine(businessAccount.Balance);//acessando com protected
+
