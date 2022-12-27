b7d5d41b0ed8542a929e12357e12f0f3b741345b
Marcos Paulo Ribeiro <31698863+mpr3421account@users.noreply.github.com>
2022-12-27 17:23:25 -0300

    Upcasting/Downcasting

--- Program.cs
+++ Program.cs
@@ -1,5 +1,19 @@
 ﻿using demoInheritance.Entities;
 
-BusinessAccount businessAccount = new BusinessAccount(8010, "Marcos", 1000.00, 500.00);
-Console.WriteLine(businessAccount.Balance);//acessando com protected
+
+Account acc = new Account(1001,"Alex", 0.0);
+BusinessAccount bacc = new BusinessAccount(1002,"Maria",0.0, 500.0);
+
+//Upcasting:
+
+Account acc1 = bacc;
+Account acc2 = new BusinessAccount(1004, "Bob", 0.0, 200.00);
+Account acc3 = new SavingsAccount(1005, "Anna", 0.0, 0.01);
+
+//Downcasting:
+BusinessAccount acc4 = (BusinessAccount)acc2;
+acc4.Loan(100.0);
+acc2.Loan(100.0);//Sem o downcasting gera um erro, porque o acc2 ainda é um tipo Account
+
+
 
