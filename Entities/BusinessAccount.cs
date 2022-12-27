0afae4f97bb328da735eb3970e984e1a5c4cca42
Marcos Paulo Ribeiro <31698863+mpr3421account@users.noreply.github.com>
2022-12-27 11:05:12 -0300

    Account class.

---
+++ Entities/BusinessAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoInheritance.Entities
+{
+    internal class BusinessAccount : Account//herança de account com (: Account)
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount() {
+
+        }
+
+        public BusinessAccount(int number, string holder, double balance) : base(number, holder, balance)
+        {
+        }
+    }
+
+
+}
