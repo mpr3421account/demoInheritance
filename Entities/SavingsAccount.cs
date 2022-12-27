2d162aa6308f0a2e455307353bb503b0a17caf0c
Marcos Paulo Ribeiro <31698863+mpr3421account@users.noreply.github.com>
2022-12-27 17:28:51 -0300

    Teste de casting com if is

--- Entities/SavingsAccount.cs
+++ Entities/SavingsAccount.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace demoInheritance.Entities
 {
-    internal class SavingsAccount
+    internal class SavingsAccount : Account
     {
+        public double InterestRate { get; set; }
+        public SavingsAccount() { }
+
+        public SavingsAccount(int number, string holder, double balance, double interestrate) : base(number, holder, balance)
+        {
+            InterestRate = interestrate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -13,7 +13,20 @@ Account acc3 = new SavingsAccount(1005, "Anna", 0.0, 0.01);
 //Downcasting:
 BusinessAccount acc4 = (BusinessAccount)acc2;
 acc4.Loan(100.0);
-acc2.Loan(100.0);//Sem o downcasting gera um erro, porque o acc2 ainda é um tipo Account
+//acc2.Loan(100.0);//Sem o downcasting gera um erro, porque o acc2 ainda é um tipo Account
 
+//BusinessAccount acc5 = (BusinessAccount)acc3;//Não dá erro, porque o compilador não consegue prever isso, mas o erro ocorre na execução
 
-
+//uma maneira de evitar o erro é testar - com if - is
+if(acc3 is BusinessAccount)
+{
+    BusinessAccount acc5 = (BusinessAccount)acc3;
+    acc5.Loan(200.0);
+    Console.WriteLine("Loan!");
+}
+if(acc3 is SavingsAccount)
+{
+    SavingsAccount acc5 = (SavingsAccount)acc3;
+    acc5.UpdateBalance();
+    Console.WriteLine("Update!");
+}
