0b1f04836088f23bf8649720a36216466fcef431
Marcos Paulo Ribeiro <31698863+mpr3421account@users.noreply.github.com>
2022-12-27 11:14:42 -0300

    Loan method

--- Entities/BusinessAccount.cs
+++ Entities/BusinessAccount.cs
@@ -11,12 +11,21 @@ namespace demoInheritance.Entities
     {
         public double LoanLimit { get; set; }
 
-        public BusinessAccount() {
+        public BusinessAccount() 
+        {
 
         }
 
-        public BusinessAccount(int number, string holder, double balance) : base(number, holder, balance)
+        public BusinessAccount(int number, string holder, double balance, double loanLimit) : base(number, holder, balance)
         {
+            LoanLimit = loanLimit;
+        }
+        public void Loan(double amount)
+        {
+            if(amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
         }
     }
 
