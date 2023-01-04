053da8c065cbabb5c3b5b1c9546fb86c32a62438
Marcos Paulo Ribeiro <31698863+mpr3421account@users.noreply.github.com>
2023-01-04 14:09:50 -0300

    //Classe abstrata não pode ser instanciada

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using demoInheritance.Entities;
 
-Account acc1 = new Account(1001, "alex", 500.0);
+Account acc1 = new Account(1001, "alex", 500.0);//Classe abstrata não pode ser instanciada
 Account acc2 = new SavingsAccount(1002, "anna", 500.0, 0.01);
 
 acc1.Withdraw(10.0);
